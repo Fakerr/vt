91b0a51dd180a325378304bf34daad9cda302569
walid <berrahal.walid07@gmail.com>
2015-09-22 21:40:25 +0200

    port consoling

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/me', function(req, res, next) {
+  res.send('test me');
+});
+
 module.exports = router;
