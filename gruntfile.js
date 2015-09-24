027844259e6a31c7e49d5eacb2d630dd75f98975
walid <berrahal.walid07@gmail.com>
2015-09-24 20:27:32 +0200

    Setting jshint and csslint on grunt workflow

--- gruntfile.js
+++ gruntfile.js
@@ -21,22 +21,41 @@ module.exports = function(grunt) {
         files: [ 'views/**.*' ]
       },
       express: {
-        files:  [ 'routes/*.js' ],
-        tasks:  [ 'express:dev' ],
+        files:  [ 'routes/*.js', 'app.js', 'server.js' ],
+        tasks:  [ 'jshint', 'express:dev' ],
         options: {
           spawn: false
         }
       },
       public: {
-        files: ["public/**/*.css", "public/**/*.js"]
+        files: ["public/**/*.css", "public/**/*.js"],
+        tasks: ['jshint', 'csslint']
       }
-    }
+    },
+    jshint: {
+			all: {
+				src:['public/**/*.js'].concat(['routes/*.js',
+         'server.js', 'app.js', 'gruntfile.js']),
+				options: {
+					jshintrc: true
+				}
+			}
+		},
+		csslint: {
+			options: {
+				csslintrc: '.csslintrc',
+			},
+			all: {
+				src: ["public/**/*.css"]
+			}
+		},
   });
 
   // Load NPM tasks
 	require('load-grunt-tasks')(grunt);
   // Default task(s).
-  grunt.registerTask('default', ['express:dev']);
-  // Server task(s)
-  grunt.registerTask('server', [ 'express:dev', 'watch' ]);
+  grunt.registerTask('default', ['lint', 'express:dev', 'watch']);
+  // Lint task(s).
+  grunt.registerTask('lint', ['jshint', 'csslint']);
+
 };
