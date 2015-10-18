a0ca06ee90590714953af2701bc6e44d1b539f24
walid <berrahal.walid07@gmail.com>
2015-10-18 14:48:03 +0200

    add opponent on room object

--- socketCtrl.js
+++ socketCtrl.js
@@ -1,8 +1,9 @@
 
 //Room constructor.
-function Room(roomName, full) {
+function Room(roomName, full, opponent) {
   this.roomName = roomName;
   this.full = full;
+  this.opponent = opponent;
 }
 
 
@@ -17,21 +18,20 @@ exports.core = function(socket, rooms){
 
   socket.on('pseudo', function(pseudo) {
     socket.pseudo = pseudo;
-    room = searchForRoom(rooms);
+    room = searchForRoom(rooms, pseudo);
     if(room) {
       socket.join(room.roomName);
       socket.room = room;
-      console.log(room);
     }else {
       socket.join(pseudo);
-      room = new Room(pseudo, false);
-      console.log(room);
+      room = new Room(pseudo, false, 'none');
       socket.room = room;
       rooms.push(room);
     }
   });
   socket.on('vt', function(msg) {
     socket.to(socket.room.roomName).emit('vt', msg);
+    console.log(socket.room);
   });
   // Logging user disconnection
   socket.on('disconnect', function(){
@@ -45,10 +45,11 @@ exports.core = function(socket, rooms){
  * Search for free room.
  */
 
-function searchForRoom(rooms) {
+function searchForRoom(rooms, pseudo) {
   for(var i=0; i < rooms.length; i++){
     if(rooms[i].full === false){
       rooms[i].full = true;
+      rooms[i].opponent = pseudo;
       return rooms[i];
      }
    }
