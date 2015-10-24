dfed7238e76e98b8a1efa26bb95f79f4252c4490
walid <berrahal.walid07@gmail.com>
2015-10-24 21:42:52 +0200

    back old version

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('connection');
+  res.render('index');
 });
 
 /* GET index page. */
 router.get('/index', function(req, res, next) {
-  res.render('index');
+  res.render('connection');
 });
 
 module.exports = router;
