8373ebc39ee2541370765200acc88b6e82573f3a
walid <berrahal.walid07@gmail.com>
2015-11-10 22:39:34 +0100

    fixed io reference bug

--- socketCtrl.js
+++ socketCtrl.js
@@ -52,7 +52,7 @@ function Player(name, number, turn) {
  * Disconnect.
  */
 
-function disconnection(socket, room, dualRooms) {
+function disconnection(socket, io, dualRooms) {
   if(socket.room.full === true) {
     updateRoomMembers(socket, dualRooms, io);
   }else {
